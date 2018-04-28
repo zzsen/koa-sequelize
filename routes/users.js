5703cf181a119c24e7a2348596642b64e0acec35
zzs <296099296@qq.com>
2018-04-28 15:25:52 +0800

    优化userservice

--- routes/users.js
+++ routes/users.js
@@ -1,47 +1,16 @@
 const router = require('koa-router')()
-const User = require('../model').user
+// const User = require('../model').user
 const checkLogin = require('../middleware/checkLogin')
 const userservice = require('../service/userService')
 
 router.prefix('/users')
 
-router.get('/', async (ctx, next) => {
-  await checkLogin.checkLogin(ctx)
-  await ctx.redirect('/users/login')
-})
+// 注册
+router.post('/register', userservice.register)
 
-router.post('/register', async (ctx, next) => {
-  await checkLogin.checkNotLogin(ctx)
-  await userservice.checkExist(ctx)
-    .then(() => {
-      User.create({ account: ctx.request.body.account, password: ctx.request.body.account })
-      ctx.body = '注册成功！'
-    })
-    .catch(() => {
-      ctx.body = '该用户已存在'
-    })
-})
-
-router.get('/login', async (ctx, next) => {
-  await checkLogin.checkNotLogin(ctx)
-  await userservice.login(ctx)
-    .then((ret) => {
-      ctx.body = ret.msg
-    })
-    .catch((ret) => {
-      ctx.body = ret.msg
-    })
-})
-router.post('/login', async (ctx, next) => {
-  await checkLogin.checkNotLogin(ctx)
-  await userservice.login(ctx)
-    .then((ret) => {
-      ctx.body = ret.msg
-    })
-    .catch((ret) => {
-      ctx.body = ret.msg
-    })
-})
+// 登录
+router.get('/login', userservice.login)
+router.post('/login', userservice.login)
 
 router.get('/signin', (ctx, next) => {
   console.log('4')
--- service/userService.js
+++ service/userService.js
@@ -1,32 +1,63 @@
+const checkLogin = require('../middleware/checkLogin')
+// 检查用户是否不存在
+let checkNotExist = (ctx) =>
+  new Promise((resolve, reject) => {
+    User.findOne({
+      where: { account: ctx.request.body.account }
+    }).then(user => {
+      if (user) {
+        reject(new Error('该用户已存在'))
+      } else {
+        resolve()
+      }
+    })
+  })
+
+// 登录
+let login = (ctx) => new Promise((resolve, reject) => {
+  var acct = ctx.request.body.acct || ctx.request.query.acct
+  var pwd = ctx.request.body.pwd || ctx.request.query.pwd
+  User.findOne({
+    where: {
+      account: acct
+    }
+  }).then(user => {
+    if (!user) {
+      reject(new Error('用户不存在！'))
+    } else if (user.password !== pwd) {
+      reject(new Error('密码错误！'))
+    }
+    // delete user.password
+    // console.log(user.password)
+    // console.log(pwd)
+    ctx.session.user = user
+    resolve({ message: '登录成功！' })
+  })
+})
 
 const User = require('../model').user
 module.exports = {
-  // 已经登录了
-  checkExist: (ctx) => new Promise((resolve, reject) => {
-    User.findOne({
-      where: { account: ctx.request.body.acct }
-    }).then(user => {
-      if (user) {
-        reject(false)
-      } else {
-        resolve(true)
-      }
-    })
-  }),
-  login: (ctx) => new Promise((resolve, reject) => {
-    var acct = ctx.request.body.acct || ctx.request.query.acct
-    var pwd = ctx.request.body.pwd || ctx.request.query.pwd
-    User.findOne({
-      where: {
-        account: acct
-      }
-    }).then(user => {
-      if (!user) { reject({ success: false, msg: '用户不存在！' }) } else if (user.password !== pwd) { reject({ success: false, msg: '密码错误！' }) }
-      // delete user.password
-      // console.log(user.password)
-      // console.log(pwd)
-      ctx.session.user = user
-      resolve({ success: true, msg: '登录成功！' })
-    })
-  })
+  // 注册
+  register: async (ctx, next) => {
+    await checkLogin.checkNotLogin(ctx)
+    await checkNotExist(ctx)
+      .then(() => {
+        User.create({ account: ctx.request.body.account, password: ctx.request.body.account })
+        ctx.body = '注册成功！'
+      })
+      .catch(() => {
+        ctx.body = '该用户已存在'
+      })
+  },
+  // 登录
+  login: async (ctx, next) => {
+    await checkLogin.checkNotLogin(ctx)
+    await login(ctx)
+      .then((ret) => {
+        ctx.body = ret.message
+      })
+      .catch((ret) => {
+        ctx.body = ret.message
+      })
+  }
 }
